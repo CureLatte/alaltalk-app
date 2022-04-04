431abeafb93be3ce8813428c544177023982abac
zhqmfkv <sk49229038@gmail.com>
2022-04-05 03:00:35 +0900

    배포용 수정

--- alaltalk/asgi.py
+++ alaltalk/asgi.py
@@ -8,15 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alaltalk.settings")
+django.setup()
 
-from channels.auth import AuthMiddlewareStack
 from channels.sessions import SessionMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
 import chat.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alaltalk.settings")
 
 # chat.router를 root router로 설정
 application = ProtocolTypeRouter(
